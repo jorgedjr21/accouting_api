26b273eb7df815c1bed37882c5d9d9082e24b597
Jorge Junior <jorgedjr21@gmail.com>
2020-05-08 18:19:33 -0300

    adds comments

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+# Handle the accounts requests
 class AccountsController < ApplicationController
   before_action :set_account, only: :create
 
+  # POST accounts/ create new account
   def create
     return render json: { message: "Account #{@account.id} already exists", account_id: @account.id, auth_token: @account.auth_token }, status: :ok if @account.present?
 
     new_account = Account.new(account_params)
     if new_account.save
-      render json: { message: 'New account was created!', account_id: new_account.id, auth_token: new_account.auth_token }, status: :created
+      render json: { message: "The account #{new_account.id} was created!", account_id: new_account.id, auth_token: new_account.auth_token }, status: :created
     else
       render json: { errors: new_account.errors }, status: :bad_request
     end
